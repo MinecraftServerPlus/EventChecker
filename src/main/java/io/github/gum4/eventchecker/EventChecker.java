10b3fa84c144f1de107ca54165165735a53335e4
9um4 <sweetenpotato@outlook.com>
2023-08-21 23:35:23 +0900

    feat: Display MoveEvent Attributes

--- src/main/java/io/github/gum4/eventchecker/EventChecker.java
+++ src/main/java/io/github/gum4/eventchecker/EventChecker.java
@@ -1,13 +1,14 @@
 package io.github.gum4.eventchecker;
 
+import io.github.gum4.eventchecker.listeners.MoveListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EventChecker extends JavaPlugin {
-
+    private final MoveListener moveListener = new MoveListener();
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getPluginManager().registerEvents(moveListener, this);
     }
 
     @Override
--- src/main/java/io/github/gum4/eventchecker/listeners/MoveListener.java
+++ src/main/java/io/github/gum4/eventchecker/listeners/MoveListener.java
@@ -1,13 +1,38 @@
 package io.github.gum4.eventchecker.listeners;
 
+import net.kyori.adventure.text.Component;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.permissions.Permission;
 
 public class MoveListener implements Listener {
+
+    public boolean activated = true;
+
     @EventHandler (priority = EventPriority.MONITOR)
     public void onMove(PlayerMoveEvent event) {
-        if ()
+        Player player = event.getPlayer();
+        if (activated) {
+            player.sendMessage(
+                    Component.text("===========[Event Checker]===========")
+                            .appendNewline()
+                            .append(Component.text("Name: " + event.getEventName()))
+                            .appendNewline()
+                            .append(Component.text("Player: " + player.getName()))
+                            .appendNewline()
+                            .append(Component.text("From Location: " + event.getFrom()))
+                            .appendNewline()
+                            .append(Component.text("To Location: " + event.getTo()))
+                            .appendNewline()
+                            .append(Component.text("From Block: " + event.getFrom().getBlock()))
+                            .appendNewline()
+                            .append(Component.text("To Block: " + event.getTo().getBlock()))
+                            .appendNewline()
+                            .append(Component.text("================================="))
+            );
+        }
     }
 }
