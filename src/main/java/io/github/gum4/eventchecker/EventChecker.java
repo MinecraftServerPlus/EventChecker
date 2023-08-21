f071e2bf1c01e80216e251307c94bbc58761f30f
9um4 <sweetenpotato@outlook.com>
2023-08-21 22:42:24 +0900

    init: Initialize Repository

---
+++ src/main/java/io/github/gum4/eventchecker/EventChecker.java
@@ -0,0 +1,17 @@
+package io.github.gum4.eventchecker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EventChecker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/io/github/gum4/eventchecker/listeners/MoveListener.java
@@ -0,0 +1,13 @@
+package io.github.gum4.eventchecker.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class MoveListener implements Listener {
+    @EventHandler (priority = EventPriority.MONITOR)
+    public void onMove(PlayerMoveEvent event) {
+        if ()
+    }
+}
